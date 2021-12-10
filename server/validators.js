3c2ff1ac339f3c1862d4bfad73f2cc211d8635f4
Ravid Eliyahu <46108499+RavidEliyahu@users.noreply.github.com>
2021-12-10 02:43:10 +0200

    adding validation with Joi

---
+++ server/validators.js
@@ -0,0 +1,34 @@
+Joi = require('joi');
+
+module.exports = function() {
+
+    /**
+     * This function validate /randomize request body.
+     * @param {Object} body - request body to validate
+     * @returns {Object} - Joi object with details of the validation.
+     */
+    this.validateRandomize = (body) => {
+        const randomizeSchema = Joi.object({
+            n: Joi.number().min(1).max(1000).integer().required(),
+            m: Joi.number().min(1).max(1000).integer().required()
+        });
+        return Joi.validate(body, randomizeSchema);
+    }
+
+    /** This function validate /solve request body.
+     * @param {Object} body - request body to validate
+     * @returns {Object} - Joi object with details of the validation.
+     */
+    this.validateSolve = (body) => {
+        const solveSchema = Joi.object({
+            n: Joi.number().min(1).max(1000).integer().required(),
+            m: Joi.number().min(1).max(1000).integer().required(),
+            mat: Joi
+            .array()
+            .items(
+                Joi.array().items(Joi.number())
+            ).required()
+        });
+        return Joi.validate(body, solveSchema);
+    }
+}
