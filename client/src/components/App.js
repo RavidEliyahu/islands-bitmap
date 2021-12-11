acc40f5947619e99e194f833a74cfd05eb93ef6d
Ravid Eliyahu <46108499+RavidEliyahu@users.noreply.github.com>
2021-12-11 11:14:25 +0200

    extract files to be ogranized

---
+++ client/src/components/App.js
@@ -0,0 +1,198 @@
+import React, { useState, useEffect, useRef } from 'react'
+import Map from './Map'
+import "../css/App.css"
+import axios from 'axios'
+
+function App() {
+  // server paths
+  const BASE_URL = process.env.REACT_APP_BASE_URL
+  const DOCKER_URL = process.env.REACT_APP_DOCKER_URL
+
+  // loading & error handling
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+
+  // show buttons states
+  const [showRandom, setShowRandom] = useState(true)
+  const [showSolve, setShowSolve] = useState(false)
+  const [showRestart, setShowRestart] = useState(false)
+
+  // matrix output variables
+  const [mat, setMat] = useState([])
+  const [size, setSize] = useState({ n: 0, m: 0})
+  const [islands, setIslands] = useState(0)
+
+  // input
+  const bitmapInputRef = useRef()
+
+  // state managers
+  const [state, setState] = useState('randomize')
+  const [action, setAction] = useState({})
+
+  /**
+   * This use effect update the matrix state
+   * when action changes.
+   */
+  useEffect(() => {
+    async function fetchAPI() {
+      try {
+        const res = await axios(action)
+        setMat(res.data.mat)
+        if ("numOfIslands" in res.data) setIslands(res.data.numOfIslands)
+      } catch (e) {
+        setMat([])
+        setError("Ops.. Something went wrong!")
+        console.error(e)
+      }
+      setLoading(false);
+    }
+    if (action.data)
+      fetchAPI()
+  }, [action])
+
+  /**
+   * This function handles the
+   * on click event of the randomize button.
+   */
+  function handleRandomize() {
+    try {
+      const [n, m] = (bitmapInputRef.current.value).replace(/\s/g, '').split(",").map(Number);
+      if (validateInput(n, m))
+      {
+        setError("") // no errors on input
+        setSize({n, m})
+        setState("randomize")
+        setLoading(true);
+        setAction({
+          method: 'POST',
+          url: `${ DOCKER_URL || BASE_URL }/randomize`,
+          headers: { "Content-Type": "application/json" },
+          data: { n, m }
+        })
+
+        // switch button states
+        setShowRandom(false);
+        setShowSolve(true);
+      } else {
+        setError("Invalid input!!!!")
+      }
+    } catch (e) {
+      setError("Ops.. Something went wrong!")
+      console.error(e)
+    }
+  }
+
+  /**
+   * This function validates user input.
+   * @param {Number} weight - matrix weight
+   * @param {Number} height - matrix height
+   * @returns {Boolean} - height & weight are valid inputs.
+   */
+  function validateInput(weight, height) {
+    if (weight && height && Number.isInteger(weight) &&
+    weight > 0 && Number.isInteger(height) &&
+    height > 0 && height <= 1000 && weight <= 1000)
+      return true
+    return false
+  }
+
+  /**
+   * This function handles the
+   * onclick event of the solve button.
+   */
+  function handleSolve() {
+    setState("solve")
+    setLoading(true)
+    setAction({
+      method: 'POST',
+      url: `${ DOCKER_URL || BASE_URL }/solve`,
+      headers: { "Content-Type": "application/json" },
+      data: { mat, n: size.n, m: size.m }
+    })
+
+    // switch buttons
+    setShowSolve(false)
+    setShowRestart(true)
+  }
+
+  /**
+   * This function returns the app to its
+   * first state.
+   */
+  function handleRestart() {
+    setState("restart")
+    setMat([])
+    setSize({ n: 0, m: 0 })
+    setShowRestart(false)
+    setShowRandom(true)
+  }
+
+  if (loading) return (<p className="center">"Loading.."</p>)
+
+  return (
+    <>
+      <div className="center">
+        <p>
+          {
+          showRandom ?
+          "Please enter bitmap size:" :
+          `${size.n} X ${size.m}`
+          }
+        </p>
+        <Map mat={mat} size={size} state={state}></Map>
+        {
+          showRestart ?
+          <p>
+            { `FOUND ${ islands } ISLAND${ islands > 1 ? "S" : "" }!` }
+          </p> : null
+        }
+        <div className="center">
+          { showRandom ?
+            <input
+              className="app-input"
+              ref={ bitmapInputRef }
+              type="text" id="bitmap_input"
+              placeholder="Bitmap size: n, m"
+            /> : null
+          }
+        </div>
+        <div className="center">
+          {
+            showRandom ?
+            <button
+              className="app-btn"
+              type="button"
+              onClick={handleRandomize}
+            >
+              RANDOMIZE
+            </button> : null
+          }
+          {
+            showSolve ?
+            <button
+              className="app-btn"
+              type="button" onClick={handleSolve}
+            >
+              SOLVE
+            </button> : null
+          }
+          {
+            showRestart ?
+            <button
+              className="app-btn"
+              type="button"
+              onClick={handleRestart}
+            >
+              RESTART
+            </button> : null
+          }
+          {
+            error ? <p className="error">{ error }</p> : null
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Map.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Sketch from 'react-p5'
+
+export default function Map({ mat, size, state }) {
+    const LEN = 8
+    const {n, m} = size;
+    let setup = (p5, canvasParentRef) => {
+        p5.createCanvas(n * LEN, m * LEN).parent(canvasParentRef)
+    }
+
+    let draw = (p5) => {
+        p5.clear()
+        p5.background(51);
+        let colors = {}
+        for (let i = 0; i < n; i++) {
+            setTimeout(() => {
+                for (let j = 0; j < m; j++) {
+                    var x =  i * LEN;
+                    var y =  j * LEN;
+                    if (mat[i][j] !== 255 && state === "solve") {
+                        if (!(mat[i][j] in colors))
+                            colors[mat[i][j]] = p5.color(p5.random(255), p5.random(255), p5.random(255));
+                        p5.fill(colors[mat[i][j]]);
+                    } else
+                        p5.fill(mat[i][j]);
+                    p5.stroke(0);
+                    p5.rect(x, y, LEN, LEN);
+                }
+            }, Math.random() * 10)
+        }
+        p5.noLoop()
+    }
+    return (
+        <div>
+            <Sketch setup={setup} draw={draw}></Sketch>
+        </div>
+    )
+}
