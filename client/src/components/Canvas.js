f916d974c219ad2126a163bebff279e32cfa6d15
Ravid Eliyahu <46108499+RavidEliyahu@users.noreply.github.com>
2021-12-15 23:10:23 +0200

    fixing coloring bug, coloring function was generating 5 digit sometimes instead of 6, now it will always generate 6 digits and the colors will not mix

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -9,7 +9,12 @@ export default function Map({ mat, size, state }) {
         currY = 0
 
     function randomColor() {
-        return '#' + Math.floor(Math.random() * 16777215).toString(16)
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
 
     useEffect(() => {
